e0f057c1e3663c934caf3d21b8669327965ac652
jhonatantirado <jhonatantiradotiradodeep@gmail.com>
2018-06-02 12:35:32 -0500

    BigO Combinations

--- ExamenParcialADS/BigO/Combinations.cs
+++ ExamenParcialADS/BigO/Combinations.cs
@@ -8,5 +8,30 @@ namespace ExamenParcialADS.BigO
 {
     class Combinations
     {
+        public static void main(String[] args)
+        {
+            Combinations combinations = new Combinations();
+            int n = 5;
+            Console.WriteLine("# Ladder's steps: " + n);
+            Console.WriteLine("# Step combinations: " + combinations.countCombinations(n, 0));
+        }
+
+        public long countCombinations(int n, int level)
+        {
+            if (n < 0)
+            {
+                return 0;
+            }
+            if (n == 0 && level == 0)
+            {
+                return 0;
+            }
+            if (n == 0 && level > 0)
+            {
+                return 1;
+            }
+            return countCombinations(n - 1, level + 1) + countCombinations(n - 2, level + 1)
+                    + countCombinations(n - 3, level + 1);
+        }
     }
 }
